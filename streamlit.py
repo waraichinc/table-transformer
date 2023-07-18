40ee9265beb1083ff3843d266606428d170ce9c8
waraichinc <jass.waraich@icloud.com>
2023-07-18 17:26:58 -0400

    add streamlit config

--- streamlit.py
+++ streamlit.py
@@ -1,6 +1,19 @@
 import streamlit as st
 
+
+st.set_page_config(
+    page_title="Table Transformer",
+    layout='wide'
+)
+
 st.title('Table Transformer')
 
 template_upload = st.file_uploader("Upload Template",type=['csv'])
 csv_upload = st.file_uploader("Upload CSV",type=['csv'])
+
+if template_upload and csv_upload:
+    None
+
+else:
+    st.write("Template and csv to be transformed are required")
+    
