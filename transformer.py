faf5cce93f8f8234a6acbba57e9f2993b16fe0de
waraichinc <jass.waraich@icloud.com>
2023-07-19 10:11:44 -0400

    add transformer that runs LLM

---
+++ transformer.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from langchain.chains import LLMChain
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import PromptTemplate
+
+class Transformer:
+    None
+    
